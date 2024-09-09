6e2ad8d19624ce5b31fbafc26674ac4da05e1d2c
Ngoc Trung <ngoctrung2k42203@gmail.com>
2024-09-09 16:15:36 +0700

    properties

---
+++ properties-configuration/src/main/java/com/example/properties_configuration/PropertiesConfigurationApplication.java
@@ -0,0 +1,26 @@
+package com.example.properties_configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PropertiesConfigurationApplication {
+	@Value("${university.name}")
+	public String ten;
+	@Value("${university.hub}")
+	public String khoa;
+	@Value("${university.address}")
+	public String address;
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesConfigurationApplication.class, args);
+	}
+	@GetMapping("/")
+	public String Print()
+	{
+		return "Nihongo ga omoshiroi";
+	}
+}
---
+++ properties-configuration/src/test/java/com/example/properties_configuration/PropertiesConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.properties_configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
